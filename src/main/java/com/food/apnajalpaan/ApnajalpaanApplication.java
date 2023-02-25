786684768fda3081c37a6ac282c874722b4e9e6b
SKUM149 <Saurav.Kumar@albertsons.com>
2023-02-25 17:15:16 +0530

    first commit

---
+++ src/main/java/com/food/apnajalpaan/ApnajalpaanApplication.java
@@ -0,0 +1,13 @@
+package com.food.apnajalpaan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApnajalpaanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApnajalpaanApplication.class, args);
+	}
+
+}
