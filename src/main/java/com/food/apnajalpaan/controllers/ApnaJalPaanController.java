2c0bfa055b4e65149886f68d2f56ef14d1dd0ec0
SKUM149 <Saurav.Kumar@albertsons.com>
2023-03-02 09:14:12 +0530

    endpoint for admin added

--- src/main/java/com/food/apnajalpaan/controllers/ApnaJalPaanController.java
+++ src/main/java/com/food/apnajalpaan/controllers/ApnaJalPaanController.java
@@ -12,33 +12,32 @@ import java.util.Map;
 
 @Slf4j
 @RestController
-@RequestMapping("/admin")
 public class ApnaJalPaanController {
     @Autowired
     AdminService adminService;
 
-    @GetMapping()
+    @GetMapping("/admin")
     public Flux<AdminModel> getAllAdmin(){
         return adminService.getAllAdmin();
     }
 
-    @GetMapping("/{adminId}")
+    @GetMapping("/admin/{adminId}")
     public Mono<AdminModel> getAdminByAdminId(@PathVariable String adminId){
         return adminService.getAdminByAdminId(adminId);
     }
 
-    @PostMapping("/register")
+    @PostMapping("/admin/register")
     public Mono<AdminModel> saveAdmin(@RequestBody Mono<AdminModel> adminModelRequest){
         return adminService.saveAdmin(adminModelRequest);
     }
 
-    @PutMapping("/update/{adminId}")
+    @PutMapping("/admin/update/{adminId}")
     public Mono<AdminModel> updateAdmin(@RequestBody Mono<AdminModel> adminModelRequest, @PathVariable String adminId)
     {
         return adminService.updateAdmin(adminId,adminModelRequest);
     }
 
-    @DeleteMapping("/delete/{adminId}")
+    @DeleteMapping("/admin/delete/{adminId}")
     public Mono<String> deleteAdmin(@PathVariable String adminId){
         return adminService.deleteAdmin(adminId);
     }
